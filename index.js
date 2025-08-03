703b13ad239abf1894cd3d17add508036687931f
kauamassei <gameplayultra11@gmail.com>
2025-08-03 18:43:12 -0300

    feat add crud functions

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // const { Person } = require("./person");
-const connectToDatabase = require("./src/connect");
+const connectToDatabase = require("./src/database/connect");
 const dotenv = require("dotenv");
 
 dotenv.config();
 connectToDatabase();
 // const person = new Person("Kauã");
-// require("./modules/express");
+require("./modules/express");
 
 // console.log(person.sayMyName());
